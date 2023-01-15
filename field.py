9a19330ef06057e6a01a2def040b9bf207d339e7
workm <workmail2006@bk.ru>
2023-01-15 22:21:19 +0300

    Добавлен фон и редндер поля

--- field.py
+++ field.py
@@ -1,4 +1,26 @@
 import pygame
 
+WIDTH = 10
+HEIGHT = 10
+CELL_SIZE = 40
+FIELD_COLOR = pygame.Color('white')
+
+
 class Field:
-    pass
+    def __init__(self, top, left):
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.board = [[0] * self.width for _ in range(self.height)]
+        self.left = left
+        self.top = top
+        self.cell_size = CELL_SIZE
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, FIELD_COLOR, (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size), 1)
+
+    def getFieldWidth(self):
+        return self.cell_size * self.width
+
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import pygame
 import sys
 
+from field import Field
+
+
 def main():
     pygame.init()
-    pass
+    bg = pygame.image.load("data/sprites/background.png")
+    screen = pygame.display.set_mode((1000, 600))
+    board1 = Field(10, 100)
+    board2 = Field(10, board1.getFieldWidth() + 150)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        screen.blit(bg, (0, 0))
+        board1.render(screen)
+        board2.render(screen)
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
